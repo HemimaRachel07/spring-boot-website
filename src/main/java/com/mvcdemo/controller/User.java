e9e1a21cd922a2fdec1c3f5ebcb1a4085db0d8b8
RachelHemima <116056640+RachelHemima@users.noreply.github.com>
2024-03-15 18:38:35 +0530

    first commit

---
+++ src/main/java/com/mvcdemo/controller/User.java
@@ -0,0 +1,19 @@
+package com.mvcdemo.controller;
+
+public class User {
+String name;
+String email;
+
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
